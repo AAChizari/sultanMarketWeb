e897a700ca0847858bf645415bc42fbb0067dcf6
unknown <amir.chizari.ac@gmail.com>
2024-08-23 15:55:59 +0200

    logothicker reperiert

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,49 +1,46 @@
 "use client";
-import ghazallogo from "@/assets/logo-ghazal.png";
-import naderilogo from "@/assets/logo-naderi.png";
-import pamirLogo from "@/assets/logo-pamir.png";
+
+import oneandone from "@/assets/logo-oneandonegroup.png";
 import cheetos from "@/assets/logo-cheetos.png";
+import ghazal from "@/assets/logo-ghazal.png";
+import naderi from "@/assets/logo-naderi.png";
+import pamir from "@/assets/logo-pamir.png";
+import ahmad from "@/assets/logo-ahmad.png";
+import { log } from "console";
 import { motion } from "framer-motion";
-import logo1n1 from "@/assets/logo-oneandonegroup.png";
-import ahmadLogo from "@/assets/logo-ahmad.png";
-import { FrameEffect } from "@/components/FrameEffect";
 
 export const LogoTicker = () => {
   return (
-    <section className="py-20 md:py-24 mt-10 overflow-hidden">
-      <div className="w-full">
-        <div className="flex items-center">
-          <div className="w-full overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+    <section className="py_20 md:py-24">
+      <div className="container">
+        <div className="flex items-center gap-5">
+
+          <div className=" flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
             <motion.div
               initial={{ translateX: "-50%" }}
-              animate={{ translateX: "0%" }}
+              animate={{ translateX: "0" }}
               transition={{
                 repeat: Infinity,
-                ease: "linear",
                 duration: 10,
+                ease: "linear",
               }}
-              className="flex flex-none gap-14 pr-14 -translate-x-1/2"
+              className="flex flex-none gap-14  pr-14 -translate-y-1/2"
             >
               {[
+                oneandone,
                 cheetos,
-                ghazallogo,
-                naderilogo,
-                pamirLogo,
-                ahmadLogo,
-                logo1n1,
-                ghazallogo,
-                naderilogo,
-                pamirLogo,
-                ahmadLogo,
-                logo1n1,
+                ghazal,
+                naderi,
+                pamir,
+                ahmad,
+                oneandone,
                 cheetos,
-                ghazallogo,
-                naderilogo,
-                pamirLogo,
-                ahmadLogo,
-                logo1n1,
+                ghazal,
+                naderi,
+                pamir,
+                ahmad,
               ].map((logo) => (
-                <img src={logo.src} key={logo.src} className="h-40 w-40" />
+                <img src={logo.src} key={logo.src} className="h-40 w-auto " />
               ))}
             </motion.div>
           </div>
